9c1237fdc8778277186cdf4f2c222afd5f68ade8
Mika Notarnicola <mika@beardphantom.com>
2022-08-01 19:13:02 -0700

    Add ReadOnlyAttribute.cs

---
+++ Packages/com.beardphantom.unity-extended/Editor/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BeardPhantom.UnityExtended.Editor
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyPropertyDrawer : PropertyDrawer
+    {
+        #region Methods
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            using (new EditorGUI.DisabledScope(true))
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        #endregion
+    }
+}
---
+++ Packages/com.beardphantom.unity-extended/Runtime/ReadOnlyAttribute.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+namespace BeardPhantom.UnityExtended
+{
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
