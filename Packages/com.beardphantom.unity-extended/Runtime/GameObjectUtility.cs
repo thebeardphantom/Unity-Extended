2533cd92ef59d7b691e019afc1a78c54b3abbcd6
Mika Notarnicola <mika@beardphantom.com>
2023-04-10 21:57:41 -0700

    Added GameObjectUtility.cs

---
+++ Packages/com.beardphantom.unity-extended/Runtime/GameObjectUtility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace BeardPhantom.UnityExtended
+{
+    public static class GameObjectUtility
+    {
+        #region Methods
+
+        public static TComponent FindWithTag<TComponent>(string tag) where TComponent : Component
+        {
+            var gameObject = GameObject.FindWithTag(tag);
+            if (gameObject.IsNull())
+            {
+                return default;
+            }
+
+            return typeof(TComponent) == typeof(Transform)
+                ? gameObject.transform as TComponent
+                : gameObject.GetComponent<TComponent>();
+        }
+
+        #endregion
+    }
+}
