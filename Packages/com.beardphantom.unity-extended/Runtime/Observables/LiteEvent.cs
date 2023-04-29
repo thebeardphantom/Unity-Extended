a8f869fb2121b49df4dae4b774ffe5cef20ebb49
Mika Notarnicola <mika@beardphantom.com>
2023-04-29 03:07:27 -0700

    Reorganize. Rewrite some of LiteEvent.

---
+++ Packages/com.beardphantom.unity-extended/Runtime/Observables/LiteEvent.cs
@@ -0,0 +1,74 @@
+using UnityEngine.Pool;
+
+namespace BeardPhantom.UnityExtended
+{
+    public sealed class LiteEvent : LiteEventBase<LiteEvent.OnEventInvoked>
+    {
+        #region Types
+
+        public delegate void OnEventInvoked();
+
+        #endregion
+
+        #region Events
+
+        public event OnEventInvoked Event
+        {
+            add => Register(value);
+            remove => Unregister(value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Invoke()
+        {
+            using (ListPool<OnEventInvoked>.Get(out var listenersCopy))
+            {
+                listenersCopy.AddRange(Listeners);
+                foreach (var listener in listenersCopy)
+                {
+                    listener();
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    public sealed class LiteEvent<TArgs> : LiteEventBase<LiteEvent<TArgs>.OnEventInvoked> where TArgs : struct
+    {
+        #region Types
+
+        public delegate void OnEventInvoked(in TArgs args);
+
+        #endregion
+
+        #region Events
+
+        public event OnEventInvoked Event
+        {
+            add => Register(value);
+            remove => Unregister(value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Invoke(in TArgs args)
+        {
+            using (ListPool<OnEventInvoked>.Get(out var listenersCopy))
+            {
+                listenersCopy.AddRange(Listeners);
+                foreach (var listener in listenersCopy)
+                {
+                    listener.Invoke(args);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Packages/com.beardphantom.unity-extended/Runtime/Observables/LiteEventBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BeardPhantom.UnityExtended
+{
+    public abstract class LiteEventBase<TDelegate> : IEnumerable<TDelegate>, ILiteEvent<TDelegate> where TDelegate : Delegate
+    {
+        #region Fields
+
+        protected readonly HashSet<TDelegate> Listeners = new();
+
+        #endregion
+
+        #region Properties
+
+        /// <inheritdoc />
+        public int ListenerCount => Listeners.Count;
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        public void Clear()
+        {
+            Listeners.Clear();
+        }
+
+        /// <inheritdoc />
+        public bool Register(TDelegate listener)
+        {
+            return Listeners.Add(listener);
+        }
+
+        /// <inheritdoc />
+        public bool Unregister(TDelegate listener)
+        {
+            return Listeners.Remove(listener);
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<TDelegate> GetEnumerator()
+        {
+            return Listeners.GetEnumerator();
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)Listeners).GetEnumerator();
+        }
+
+        #endregion
+    }
+}
